653986017073d0c57833073df072c405551f46f3
fitboye <49235923+fitboye@users.noreply.github.com>
2022-11-14 22:13:47 -0500

    Hud Elements Added
    Hud now displays players current money and the attack selection slider.

--- countryObject.py
+++ countryObject.py
@@ -5,6 +5,7 @@ class country:
         self.color = color #fill color on board
         self.money = 0
         self.size = 0
+        self.attackProportion = 0.5 #Must be between 0 and 1 inclusive
     
     def __str__(self):
         return str(self.id)+' '+str(self.color)
--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def appStarted(app):
     app.cellSize = 10
     app.rows = 80
     app.cols = 80
+    app.hudHeight = app.height-app.cols*app.cellSize
+    #sliderBox is the coordinates for the bounding box of the attack slider
+    app.sliderBox = (app.width*0.35,
+                    app.cols*app.cellSize+app.hudHeight/2-app.hudHeight*0.3,
+                    app.width*7/8,
+                    app.cols*app.cellSize+app.hudHeight/2+app.hudHeight*0.3)
     app.players = 1
     app.defaultFill = "#1A1A1A"
     app.countryColors = ["blue"] #To be expanded in the future
@@ -49,8 +55,36 @@ def drawBoard(app,canvas):
                 fill = app.dict[id].color
             canvas.create_rectangle(x0,y0,x1,y1,fill=fill,outline=fill)
 
+def sliderColor(player):
+    """when proportion is 0.0, slider is green, when proportion is 1.0,
+    the slider is red. The color ranges from green to red depending
+    on the proportion selected"""
+    r = int(255*player.attackProportion)
+    g = int(255*(1-player.attackProportion))
+    b = int(100*(1-player.attackProportion))
+    return "#"+hex(r)[2:]+hex(g)[2:]+hex(b)[2:]
+
 def drawHud(app,canvas):
-    return
+    #Hud border with game board
+    canvas.create_line(0,app.cellSize*app.cols,app.width,app.cellSize*app.cols,
+    fill = "red",width = 3)
+
+    #Displaying player money
+    #The id of the player is always 0
+    player = app.dict[0]
+    font=('Comic Sans MS', 20, 'bold italic')
+    x, y = app.width/8, app.cellSize*app.cols+app.hudHeight/2
+    canvas.create_text(x, y, text=f'{player.money}', fill='green', font=font)
+
+    #Displaying slider
+    x0,y0,x1,y1 = app.sliderBox
+    canvas.create_rectangle(x0,y0,x1,y1,outline="black",fill="#303030")
+    canvas.create_rectangle(x0,y0,x0+(x1-x0)*player.attackProportion,y1,
+    fill=sliderColor(player),outline="black")
+    attackMon = int(player.money*player.attackProportion)
+    canvas.create_text((x0+x1)/2, (y0+y1)/2, text=f'{attackMon}', 
+    fill='white', font=font)
+
 
 def keyPressed(app, event):
     return
@@ -60,6 +94,7 @@ def timerFired(app):
 
 def redrawAll(app, canvas):
     drawBoard(app, canvas)
+    drawHud(app, canvas)
 
 def runGame():
     width, height = dimensions()
